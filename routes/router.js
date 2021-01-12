7e4ed16203bf47eb968a000be5b6eaa84e9b1e2f
bhupender <bhupender.cool98@gmail.com>
2021-01-12 13:24:54 +0530

    changes have been made

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const {runQuery} = require('./config/db')
+const {runQuery} = require('../config/db')
 
 router.get("/getusers", async (req,res)=>{
     const executeQuery = await runQuery(`select * from user`,[])
