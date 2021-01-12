ffcd34d1ca4bfc5d4099a6495a517257818f7719
bhupender <bhupender.cool98@gmail.com>
2021-01-12 16:17:10 +0530

    it contains middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+// const { body ,validationResult} =require('express-validator')
+// const validate=function(req,res,next){
+//    console.log('this is bhupi')
+//     body('username').isEmail(),
+//     body('password').isLength({min:4})
+//     // const errors=validationResult(req)
+//     // if(!errors.isEmpty()){
+//     //     return res.status(400).json({message:errors.array()})
+//     // }
+//     next()
+// }
+
+// module.exports={validate}
